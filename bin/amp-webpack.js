1f164aa367f785eff00f04bf0791cb98df062f89
polpenaloza <polpenaloza@gmail.com>
2020-04-07 22:14:59 +0200

    fix bin

--- bin/amp-webpack.js
+++ bin/amp-webpack.js
@@ -3,16 +3,11 @@ const yargs = require('yargs');
 const devBuild = require('../src/dev.babel');
 const prodBuild = require('../src/prod.babel');
 
-const { argv } = yargs
-  .usage(
-    '$0 [options]',
-    'Syncs a local directory to an AWS S3 bucket, optionally invalidating affected CloudFront paths.'
-  )
-  .option('env', {
-    type: 'string',
-    describe: 'Environment for build',
-    default: 'dev',
-  });
+const { argv } = yargs.usage('$0 [options]', 'Build the project depending on the desired env.').option('env', {
+  type: 'string',
+  describe: 'Environment for build',
+  default: 'dev',
+});
 
 if (argv.env === 'prod') {
   prodBuild();
