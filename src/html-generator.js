4189c075db69183157ab614c585c5e140f4546e1
polpenaloza <polpenaloza@gmail.com>
2020-04-09 20:51:24 +0200

    support hbs by default

--- src/html-generator.js
+++ src/html-generator.js
@@ -4,7 +4,7 @@ const htmlGenerator = (mapJS) => {
   return Object.entries(mapJS).map(
     ([source, { filename, chunks }]) =>
       new HtmlWebpackPlugin({
-        template: source,
+        template: source.indexOf('.') === -1 ? `${source}.hbs` : source,
         filename,
         chunks: ['runtime', 'vendors', ...chunks],
         scriptLoading: 'defer',
