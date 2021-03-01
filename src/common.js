f02cb0b3663608dc1ae5827d5e114b4d976bfd80
Jeff Palmeri <jeffpalmeri184@gmail.com>
2021-03-01 16:56:45 -0500

    fix(path): edit main js path

--- src/common.js
+++ src/common.js
@@ -59,7 +59,7 @@ const common = (init) => {
   const output = {
     path: path.join(process.cwd(), 'dist'),
     publicPath: '/ampstaging/',
-    filename: 'js/[name].min.js',
+    filename: 'ampstaging/js/[name].min.js',
   };
 
   const optimization = {
